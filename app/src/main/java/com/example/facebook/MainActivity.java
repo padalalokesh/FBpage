f1c4eaef41ed25ab4f194d491783fb6ca36b648b
padalalokesh <padalalokesh1999@gmail.com>
2020-04-29 12:58:49 +0530

    update upto (29/04/20)

---
+++ app/src/main/java/com/example/facebook/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.facebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Button reg =findViewById(R.id.regbut);
+         reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}
